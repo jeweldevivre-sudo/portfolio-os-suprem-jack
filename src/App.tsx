9a5bcbea0ad435dbf6745169cff837b5f0ff6a0a
jeweldevivre-sudo <jeweldevivre@gmail.com>
2026-05-16 17:29:10 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useMemo, useState } from "react";
 
 const SCRIPT_URL =
   (process as any).env?.REACT_APP_PREMIUM_SCRIPT_URL ||
-  "https://script.google.com/macros/s/AKfycbx954UIlvG-ox64w1zEHpxQhWqzl0IYgSuw4krXXNvgpcbCnJFPN6DPPx__wJcXSbNK8A/exec";
+  "https://script.google.com/macros/s/AKfycbwSqc5n_YNNKKfkfvHLXRteqPD6dwcHp5ELZLtwUh8PfncsVmfO21iWA_scdVbUrSrm/exec";
 
 const EMPTY_DATA = {
   summary: {},
